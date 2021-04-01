d73fd1b572983e453738dc172adb10d2b0d70154
domenicolorusso <lorusso.dome5@gmail.com>
2021-04-01 16:30:03 +0200

    feat: UI table implemented

--- src/components/DetailsTable.js
+++ src/components/DetailsTable.js
@@ -3,7 +3,22 @@ import React from 'react'
 function DetailsTable() {
     return (
         <div className='DetailsTable'>
-            DETAILS TABLE
+            <div className='detailsSection'>
+                <h5>Prezzo Veicolo</h5>
+                <p>1000 €</p>
+            </div>
+            <div className='detailsSection'>
+                <h5>Anticipo</h5>
+                <p>1000 €</p>
+            </div>
+            <div className='detailsSection'>
+                <h5>Numero rate</h5>
+                <p>1000 €</p>
+            </div>
+            <div className='detailsSection'>
+                <h5>Importo rate</h5>
+                <p>1000 €</p>
+            </div>
         </div>
     )
 }
