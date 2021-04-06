9efc3fdf7fa5197e9484072593d538fee0f70da8
domenicolorusso <lorusso.dome5@gmail.com>
2021-04-06 16:40:25 +0200

    feat: form handler without redux

--- src/components/ContactMe/ContactForm.js
+++ src/components/ContactMe/ContactForm.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ContactForm() {
+function ContactForm({handleFormInput}) {
+
+  const [validate, setValidate] = useState(false)
   return (
     <div className="ContactForm">
       <h4>Invia Richesta</h4>
@@ -8,23 +10,23 @@ function ContactForm() {
       <form>
         <div className="formField">
           <label htmlFor='name'>Nome*</label>
-          <input type="text" id='name' required />
+          <input type="text" id='name' name='name' onChange={handleFormInput}/>
         </div>
         <div className="formField">
           <label htmlFor='surname'>Cognome*</label>
-          <input type="text" id='surname' required />
+          <input type="text" id='surname' name='surname' onChange={handleFormInput} />
         </div>
         <div className="formField">
           <label htmlFor='telNumber'>Cellulare*</label>
-          <input type="text" id='telNumber' required />
+          <input type="text" id='telNumber' name='telNumber' onChange={handleFormInput} />
         </div>
         <div className="formField">
           <label htmlFor='email'>Indirizzo E-mail*</label>
-          <input type="email" id='email' required />
+          <input type="email" id='email' name='email'  onChange={handleFormInput}  />
         </div>
         <div className="formField">
           <label htmlFor='confirmEmail'>Conferma E-mail*</label>
-          <input type="email" id='confirmEmail' required />
+          <input type="email" id='confirmEmail' name='confirmEmail' onChange={handleFormInput}/>
         </div>
       </form>
       <p>*campi obbligatori</p>
--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import Btn from "../Btn/Btn";
 import ContactForm from "./ContactForm";
+
 function ContactMe() {
+  const [userData, setUserData ] = useState({})
+
+
+  const handleFormInput = (e) => {
+    setUserData( user => ({...user, [e.target.name]: e.target.value}))
+    console.log(userData)
+  }
   const section = "CONTACTME";
   return (
     <div className="ContactMe">
       <div className="generalWrapper">
-        <ContactForm />
+        <ContactForm handleFormInput={handleFormInput} userData={userData}/>
         <div className="infoPrivacy">
           <h4>INFORMATIVA PRIVACY</h4>
           <p>
